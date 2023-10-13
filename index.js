5c5376fe16151fc53625540fba963db675649c29
el-sam <samurai.h@yahoo.com>
2023-10-13 20:49:12 +0800

    add shortGreeting

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = "Steve";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = "WWELCOME TO FLATBOOK," + welcomeMessage.toUpperCase() + "!";
+const shortGreeting = `Welcome, ${currentUser[0]}!`; 
