5fedff8387097ba93b429b3b29cfc872e8bef85b
el-sam <samurai.h@yahoo.com>
2023-10-13 20:41:33 +0800

    use '+' concat the values for  excitedWelcomeMessage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = "Steve";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = "WWELCOME TO FLATBOOK," + welcomeMessage.toUpperCase() + "!";
